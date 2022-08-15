10bb1914168a5e941124c3578ecbe31e326407cd
Gagik <gagik.hayrapetyan.00@gmail.com>
2022-08-15 11:55:10 +0400

    rename

--- command/implementation.py
+++ command/implementation.py
@@ -11,27 +11,27 @@ class SendCommand(CommandInterface):
 
     def execute(self):
         self._sock.command(self._cmd)
-        self.wait_for_response(self._cmd)
+        self._wait_for_response(self._cmd)
 
 
 class SendAddress(CommandInterface):
     def execute(self, address: int):
         self._sock.command(CMD.SEND_ADDRESS)
         self._sock.write(address.to_bytes(4, byteorder='little'))
-        self.wait_for_response(CMD.SEND_ADDRESS)
+        self._wait_for_response(CMD.SEND_ADDRESS)
 
 
 class SendData(CommandInterface):
     def execute(self, data: bytes):
         self._sock.command(CMD.SEND_DATA)
         self._sock.write(data)
-        self.wait_for_response(CMD.SEND_DATA)
+        self._wait_for_response(CMD.SEND_DATA)
 
 
 class RecvData(CommandInterface):
     def execute(self, length: int) -> Optional[bytes]:
         self._sock.command(CMD.RECV_DATA)
         data = self._sock.read(length)
-        self.wait_for_response(CMD.RECV_DATA)
+        self._wait_for_response(CMD.RECV_DATA)
 
         return data
--- command/interface.py
+++ command/interface.py
@@ -6,7 +6,7 @@ class CommandInterface(ABC):
     def __init__(self, sock: Socket) -> None:
         self._sock = sock
 
-    def wait_for_response(self, msg: str) -> None:
+    def _wait_for_response(self, msg: str) -> None:
         if not self._sock.wait_for_msg(msg):
             raise 'Failed'
 
